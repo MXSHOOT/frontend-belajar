25c056d6fe30f8682edae0510f16ebca0fffeb31
Faris Ar Rasyid <48386460+MXSHOOT@users.noreply.github.com>
2022-03-16 21:21:40 +0700

    pc
    task 4

---
+++ task-frontend/frontend-programming-pertemuan-4/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p>Copyright @mxshoot</p>
+            <small>Created by React.js</small>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ task-frontend/frontend-programming-pertemuan-4/src/components/Main.js
@@ -0,0 +1,15 @@
+import Hello from './Hello';
+
+const Main = () => {
+    return(
+        <main>
+            <Hello nama="Aufa" major="Informatika"/>
+            <Hello nama="Mikel" major="Informatika"/>
+            <Hello nama="Hannah" major="Informatika"/>
+            <Hello nama="Jonas" major="Informatika"/>
+            <Hello nama="Martha" major="Informatika"/>
+        </main>
+    )
+}
+
+export default Main;
