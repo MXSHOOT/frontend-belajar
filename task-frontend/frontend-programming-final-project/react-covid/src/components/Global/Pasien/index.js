48ef30cbb457029a511ea2062e0c755ff7ed9f8d
Faris Ar Rasyid <48386460+MXSHOOT@users.noreply.github.com>
2022-04-20 19:49:52 +0700

    update
    total

--- task-frontend/frontend-programming-final-project/react-covid/src/components/Global/Pasien/index.js
+++ task-frontend/frontend-programming-final-project/react-covid/src/components/Global/Pasien/index.js
@@ -3,9 +3,9 @@ import styles from "./Pasien.module.css";
 function Pasien(props) {
     const {pasien} = props;
     return(
-        <div className={styles.pasien }>
-            <p className={styles.pasien__status}>{pasien.status}</p>
-            <p className={styles.pasien__total}>{pasien.total}</p>
+        <div className={styles.pasien}>
+            <h1 className={styles.pasien__status}>{pasien.status}</h1>
+            <p className={styles[pasien.status]}>{pasien.total}</p>
         </div>
     );
 }
