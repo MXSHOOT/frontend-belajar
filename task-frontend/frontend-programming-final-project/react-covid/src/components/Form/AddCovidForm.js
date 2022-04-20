3f9f32c372a61738974746ed1d1eed98aca08067
Faris Ar Rasyid <48386460+MXSHOOT@users.noreply.github.com>
2022-04-20 22:30:48 +0700

    update
    addfrom

--- task-frontend/frontend-programming-final-project/react-covid/src/components/Form/AddCovidForm.js
+++ task-frontend/frontend-programming-final-project/react-covid/src/components/Form/AddCovidForm.js
@@ -8,16 +8,46 @@ console.log(paint);
 
 function AddCovidForm(props){
     const {pasiens, setPasiens} = props;
-
-    // buat variable state input form
-    const [provinsi, setProvinsi] = useState("Jakarta");
-    const [status, setStatus] = useState("Positif");
-    const [jumlah, setJumlah] = useState(0);
+    
+    const [formData, setFormData] = useState({
+        provinsi: "",
+        status: "",
+        jumlah: "",
+      });
 
     // buat  fungsi handleProvinsi
-    function handleJumlah(e){
-        setJumlah(e.target.value);
-    }
+    function handleChange(e) {
+        // Destructing name dan value.
+        const { name, value } = e.target;
+    
+        /**
+         * Mengupdate state berupa object:
+         * - Menggunakan spread operator:
+         * - Update property berdasarkan apapun nilai name.
+         */
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      }
+      function validate() {
+        if (provinsi === "") {
+            setIsProvinsiError(true);
+            return false;
+        } else if (status === "") {
+            setIsStatusError(true);
+            setIsProvinsiError(false);
+          return false;
+        } else if (jumlah === "") {
+            setIsJumlahError(true);
+            setIsStatusError(false);
+          return false;
+        } else {
+            setIsProvinsiError(false);
+            setIsStatusError(false);
+          return true;
+        }
+      }
     
 
     // state untuk error input form
@@ -25,36 +55,32 @@ function AddCovidForm(props){
     const [isStatusError, setIsStatusError] = useState(false);
     const [isJumlahError, setIsJumlahError] = useState(false);
 
-    function handleSubmit(e){
+    function addPasien() {
+        const pasien = {
+          id: nanoid(),
+          provinsi: provinsi,
+          status: status,
+          jumlah: jumlah,
+        };
+    
+        // SOLVED: HOW TO ADD MOVIE TO MOVIES :)
+        setPasiens([...pasiens, pasien]);
+      }
+    
+      function handleSubmit(e) {
         e.preventDefault();
-
-        // validasi form harus diisi
-        if(provinsi === ''){
-            setIsProvinsiError(true);
-        } else if(status === ''){
-            setIsStatusError(true);
-        } else if(jumlah === ''){
-            setIsJumlahError(true);
-        } else {
-            const pasien = {
-                id: nanoid(),
-                lokasi: provinsi,
-                kondisi: status,
-                berapa: jumlah
-            }
-            setPasiens([...pasiens, pasien]);
-
-            setIsProvinsiError(false);
-            setIsStatusError(false);
-            setIsJumlahError(false);
-        }
-    }
-
-    return(
+    
+        validate() && addPasien();
+      }
+      const {provinsi, status, jumlah} = formData;
+    
+      return (
         <div className={styles.container}>
+            <section className={styles.form}>
             <div className={styles.form__left}>
                 <img src={paint} alt="" className={styles.form__image} />
             </div>
+  
 
             <div className={styles.form__right}>
                 <h2 className={styles.form__title}>Form Covid</h2>
@@ -62,32 +88,32 @@ function AddCovidForm(props){
                 <form onSubmit={handleSubmit}>
 
                     <div className={styles.form__control}>
-                        <label htmlFor="provinsi">Provinsi</label>
-                        <select name="provinsi" id="provinsi" className={styles.form__input} value={provinsi} onChange={(e)=>setProvinsi(e.target.value)} >
-                            <option vale="jakarta">Jakarta</option>
-                            <option vale="jabar">Jawa Barat</option>
-                            <option vale="jateng">Jawa Tengah</option>
-                            <option vale="jatim">Jawa Timur</option>
-                            <option vale="banten">Banten</option>
-                            <option vale="yogyakarta">Yogyakarta</option>
-                            <option vale="riau">Riau</option>
-                            <option vale="bali">Bali</option>
-                            <option vale="sumtar">Sumatera Utara</option>
-                            <option vale="sumsel">Sumatera Selatan</option>
-                            <option vale="sultan">Sulawesi Selatan</option>
-                            <option vale="sulbar">Sumatera Barat</option>
-                            <option vale="nustetim">Nusa Tenggara Timur</option>
-                            <option vale="bangbel">Bangka Belitung</option>
-                            <option vale="lampung">Lampung</option>
-                            <option vale="kepri">Kepulauan Riau</option>
-                            <option vale="kasel">Kalimantan Selatan</option>
-                            <option vale="kalitim">Kalimantan Timur</option>
-                            <option vale="kalibar">Kalimantan Barat</option>
-                            <option vale="sulteng">Sulawesi Tengah</option>
-                            <option vale="kaliteng">Kalimantan Tengah</option>
-                            <option vale="suluta">Sulawesi Utara</option>
-                            <option vale="papua">Papua</option>
-                            <option vale="kaliuta">Kalimantan Utara</option>
+                        <label label htmlFor="provinsi" className={styles.form__label}>Provinsi</label>
+                        <select name="provinsi" id="provinsi" className={styles.form__input} value={provinsi} onChange={handleChange}>
+                            <option value="jakarta">Jakarta</option>
+                            <option value="jabar">Jawa Barat</option>
+                            <option value="jateng">Jawa Tengah</option>
+                            <option value="jatim">Jawa Timur</option>
+                            <option value="banten">Banten</option>
+                            <option value="yogyakarta">Yogyakarta</option>
+                            <option value="riau">Riau</option>
+                            <option value="bali">Bali</option>
+                            <option value="sumtar">Sumatera Utara</option>
+                            <option value="sumsel">Sumatera Selatan</option>
+                            <option value="sultan">Sulawesi Selatan</option>
+                            <option value="sulbar">Sumatera Barat</option>
+                            <option value="nustetim">Nusa Tenggara Timur</option>
+                            <option value="bangbel">Bangka Belitung</option>
+                            <option value="lampung">Lampung</option>
+                            <option value="kepri">Kepulauan Riau</option>
+                            <option value="kasel">Kalimantan Selatan</option>
+                            <option value="kalitim">Kalimantan Timur</option>
+                            <option value="kalibar">Kalimantan Barat</option>
+                            <option value="sulteng">Sulawesi Tengah</option>
+                            <option value="kaliteng">Kalimantan Tengah</option>
+                            <option value="suluta">Sulawesi Utara</option>
+                            <option value="papua">Papua</option>
+                            <option value="kaliuta">Kalimantan Utara</option>
                             <option value="aceh">Aceh</option>
                             <option value="jambi">Jambi</option>
                             <option value="nustebar">Nusa Tenggara Barat</option>
@@ -109,7 +135,7 @@ function AddCovidForm(props){
 
                     <div className={styles.form__control}>
                         <label htmlFor="status">Status</label>
-                        <select name="status" id="status" className={styles.form__input} value={status} onChange={(e)=>setStatus(e.target.value)} >
+                        <select name="status" id="status" className={styles.form__input} value={status} onChange={handleChange} >
                             <option value="positif">Positif</option>
                             <option value="sembuh">Sembuh</option>
                             <option value="dirawat">Dirawat</option>
@@ -122,15 +148,18 @@ function AddCovidForm(props){
 
                     <div className={styles.form__control}>
                         <label htmlFor="jumlah">Jumlah</label>
-                        <input id="kasus" type="number" className={styles.form__input} value={jumlah} onChange={handleJumlah} />
+                        <input id="kasus" type="number" name="jumlah" className={styles.form__input} value={jumlah} onChange={handleChange}/>
 
                         {/* jika isDateError true, maka tampilkan error */}
-                        {isJumlahError && <Alert>Year wajib diisi!</Alert>}
+                        {isJumlahError && <Alert>jumlah wajib diisi!</Alert>}
                     </div>
 
-                    <button type="submit" className={styles.form__button}>Submit</button>
+                    <div>
+                        <button className={styles.form__button}>Add Pasien</button>
+                    </div>
                 </form>
             </div>
+            </section>
         </div>
     );
 }
